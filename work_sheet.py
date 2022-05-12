19635ba7a9966b61a397154fb0f0d83354bd0d83
Omar Sayed <sayedomar74@gmail.com>
2022-05-12 18:56:45 +0200

    Update work_sheet.py

--- work_sheet.py
+++ work_sheet.py
@@ -12,6 +12,23 @@ ORDER_STATUS = 23
 #Placeholders in messangerbot sheet
 BOT_PHONE_NUMBER = 0
 PSID = 1
+
+def get_phone_by_psid(psid):
+    '''
+    Get phone number by psid
+    response:
+    phone number
+    '''
+    sh = gc.open("Woody Ordering System")
+    messange_bot = sh.worksheet("messanger-bot")
+    messange_bot_df = pd.DataFrame(messange_bot.get_all_values())
+    check_psid = messange_bot_df.loc[messange_bot_df[1] == psid]
+    if len(check_psid) > 0:
+        print(check_psid)
+        formated_phone = check_psid[0].values[0]
+        return formated_phone.replace(" ","").replace("-","").replace("(","").replace(")","")
+    else:
+        return "PSID not exist!"
 def get_status(phone):
     '''
     Get status by phone number 
@@ -21,8 +38,6 @@ def get_status(phone):
     '''
     #reformat the phone number (01x) xxx-xx-xxx
     phone = "("+str(phone[:3])+")"+" "+str(phone[3:6])+"-"+str(phone[6:8])+"-"+str(phone[8:])
-
-
     # laser = sh.worksheet("ToLaser")
     # laser_df = pd.DataFrame(laser.get_all_values())
     # # laser_df.columns = laser_df.loc[0,:].values
@@ -37,28 +52,58 @@ def get_status(phone):
     design = sh.worksheet("ToDesign")
     design_df = pd.DataFrame(design.get_all_values())
     # design_df.columns = design_df.loc[0,:].values
+
+    cancelled = sh.worksheet("Cancelled")
+    cancelled_df = pd.DataFrame(cancelled.get_all_values()) 
     check_phone = design_df.loc[design_df[PHONE_NUMBER] == phone]
     sheets=[shipped_df]
+    flag = ""
     if len(check_phone) > 0:
         #User is found
-        for sheet in sheets:
-            record = sheet.loc[sheet[PHONE_NUMBER] == phone]
-            if len(record) < 1:
-                return "Ready to be deleiverd"
-            if len(record) == 1:
-                print("Record:",record)
-                print("Status:", record[ORDER_STATUS].values)
-                if record[ORDER_STATUS].values == '':
+        # print("User is found")
+        #check if order is cancelled or not
+        check_cancelled = cancelled_df.loc[cancelled_df[PHONE_NUMBER] == phone]
+        if len(check_cancelled) > 0:
+            #User is found in cancelled orders 
+            if len(check_cancelled) == 1:
+                if check_cancelled[ORDER_STATUS].values == '':
+                    return "Order is cancelled"
+            elif len(check_cancelled) > 1:
+                if check_cancelled.iloc[-1][ORDER_STATUS] == '':
+                    return "Order is cancelled"   
+        # print("Order not cancelled")
+        #check if order is designed or not
+        if len(check_phone) == 1:
+            if check_phone[ORDER_STATUS].values == 'Designed':
+                flag = "Order is designed"
+                # print("Order is Designed")
+            else:
+                return "Ordered is confirmed"
+        elif len(check_phone) > 1:
+            if check_phone.iloc[-1][ORDER_STATUS] == 'Designed':
+                flag = "Order is designed"
+                # print("Order is Designed")
+            else:
+                return "Ordered is confirmed"
+
+        #order is designed and ready to be deleiverd
+        if flag == "Order is designed":
+            # print("order is designed and ready to be deleiverd")
+            for sheet in sheets:
+                record = sheet.loc[sheet[PHONE_NUMBER] == phone]
+                if len(record) < 1:
                     return "Ready to be deleiverd"
-                else:
-                    return record[ORDER_STATUS].values
-            elif len(record) > 1:
-                print("Status:", record.iloc[-1][ORDER_STATUS])
-                if record.iloc[-1][ORDER_STATUS] == '':
-                    if sheet == shipped_df:
+                if len(record) == 1:
+                    if record[ORDER_STATUS].values == '':
                         return "Ready to be deleiverd"
-                else:
-                    return record.iloc[-1][ORDER_STATUS]
+                    else:
+                        return record[ORDER_STATUS].values
+                elif len(record) > 1:
+                    if record.iloc[-1][ORDER_STATUS] == '':
+                        if sheet == shipped_df:
+                            return "Ready to be deleiverd"
+                    else:
+                        return record.iloc[-1][ORDER_STATUS]
     else:
         return "Not found"
 
@@ -75,13 +120,18 @@ def turn_on_tracking(phone, psid):
     record = design_df.loc[design_df[PHONE_NUMBER] == phone]
     messange_bot = sh.worksheet("messanger-bot")
     messange_bot_df = pd.DataFrame(messange_bot.get_all_values())
+    check_phone = messange_bot_df.loc[messange_bot_df[0] == phone]
     if len(record) > 0:
-        messange_bot_df.loc[-1] = [phone, psid]
-        messange_bot_df.index = messange_bot_df.index + 1
-        messange_bot_df = messange_bot_df.sort_index()
-        messange_bot.update([] + messange_bot_df.values.tolist())
-        return "Notification is turned on"
+        if len(check_phone) > 0:
+            #phone already added we only update psid
+            messange_bot_df.loc[messange_bot_df[0] == phone,1] = psid
+            messange_bot.update([] + messange_bot_df.values.tolist())
+            return "PSID is updated"
+        else:
+            messange_bot_df.loc[-1] = [phone, psid]
+            messange_bot_df.index = messange_bot_df.index + 1
+            messange_bot_df = messange_bot_df.sort_index()
+            messange_bot.update([] + messange_bot_df.values.tolist())
+            return "Notification is turned on"
     else:
         return "User not found!"
-
-turn_on_tracking("01018719246","1029764548")
