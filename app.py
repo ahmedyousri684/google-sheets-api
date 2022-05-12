62838dd3c990ef4a3476d814e8cff67bd1fcb67d
Omar Sayed <sayedomar74@gmail.com>
2022-05-12 18:56:59 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, redirect, url_for, request, jsonify
-from work_sheet import turn_on_tracking, get_status
+from work_sheet import turn_on_tracking, get_status, get_phone_by_psid
 app = Flask(__name__)
 
 
@@ -8,27 +8,26 @@ def work_sheet():
     if request.method == 'POST':
         phone = request.args.get('phone')
         psid = request.args.get('psid')
-        print("DATA",phone,psid)
         if phone and psid != None:
             api_resp = turn_on_tracking(phone, psid)
             if api_resp == "Notification is turned on":
                 resp = jsonify({'message': api_resp})
                 resp.status_code = 201
+                
             else:
                 resp = jsonify({'message': api_resp})
-                resp.status_code = 404
+                resp.status_code = 200
             return resp
         else:
             resp = jsonify({'message': 'No order '})
-            resp.status_code = 405
+            resp.status_code = 404
             return resp
     elif request.method == 'GET':
         phone = request.args.get('phone')
         api_resp = get_status(phone)
-        print(api_resp,"heree")
         if phone:
             if api_resp == "Ready to be deleiverd":
-                resp = jsonify({'Status':api_resp })
+                resp = jsonify({'message':api_resp })
                 resp.status_code = 200
                 return resp
             elif api_resp == "Not found":
@@ -36,7 +35,7 @@ def work_sheet():
                 resp.status_code = 404
                 return resp
             else:
-                resp = jsonify({'Status':api_resp[0] })
+                resp = jsonify({'message':api_resp[0] })
                 resp.status_code = 200
                 return resp
         else:
@@ -44,6 +43,29 @@ def work_sheet():
             resp.status_code = 405
             return resp
 
+@app.route('/status-by-psid', methods=['GET'])
+def get_status_psid():
+    if request.method == 'GET':
+        psid = request.args.get('psid')
+        phone = get_phone_by_psid(psid)
+        api_resp = get_status(phone)
+        if psid:
+            if api_resp == "Ready to be deleiverd":
+                resp = jsonify({'message':api_resp })
+                resp.status_code = 200
+                return resp
+            elif api_resp == "Not found":
+                resp = jsonify({'message': 'Not found'})
+                resp.status_code = 404
+                return resp
+            else:
+                resp = jsonify({'message':api_resp[0] })
+                resp.status_code = 200
+                return resp
+        else:
+            resp = jsonify({'message': 'Bad request'})
+            resp.status_code = 405
+            return resp
 if __name__ == '__main__':
     app.run()
 
